6539845d2e3b71c5d41d50ab8c58b9e9a07258fb
HattoriNK <lethalshot13@gmail.com>
2022-10-29 18:57:04 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='clean_folder',
+      version='0.9.0',
+      description='script for clean folder',
+      url='https://github.com/HattoriNK',
+      author='Hattori',
+      author_email='lethalshot13@gmail.com',
+      license='MIT',
+      packages=find_namespace_packages(),
+      entry_points={'console_scripts': ['clear-folder = clean_folder.clean:']}
+      )
